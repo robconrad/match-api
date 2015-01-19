2c462d59f0022a7ce6e778d6b5245c2c842a89e3
robert.conrad <robert.conrad@coinposit.com>
2015-01-18 16:15:56 -0800

    Remove UserServiceMock

--- project-entity/src/test/scala/base/entity/group/impl/GroupServiceImplTest.scala
+++ project-entity/src/test/scala/base/entity/group/impl/GroupServiceImplTest.scala
@@ -2,21 +2,24 @@
  * Copyright (c) 2015 Robert Conrad - All Rights Reserved.
  * Unauthorized copying of this file, via any medium is strictly prohibited.
  * This file is proprietary and confidential.
- * Last modified by rconrad, 1/17/15 6:17 PM
+ * Last modified by rconrad, 1/18/15 4:15 PM
  */
 
 package base.entity.group.impl
 
+import java.util.UUID
+
 import base.common.random.RandomService
-import base.common.service.{ TestServices, Services }
+import base.common.service.{ Services, TestServices }
 import base.common.time.mock.TimeServiceConstantMock
-import base.entity.auth.context.AuthContextDataFactory
+import base.entity.auth.context.{ AuthContext, AuthContextDataFactory }
 import base.entity.error.ApiError
+import base.entity.group.UserService
 import base.entity.group.kv.{ GroupKeyService, GroupUserKeyService }
-import base.entity.group.model.{ GroupModelBuilder, GroupModel }
+import base.entity.group.model.{ GroupModel, GroupModelBuilder }
+import base.entity.kv.Key._
 import base.entity.kv.{ KeyId, KvTest }
 import base.entity.service.EntityServiceTest
-import base.entity.user.mock.UserServiceMock
 import base.entity.user.model.UserModel
 
 import scala.concurrent.Future
@@ -40,22 +43,26 @@ class GroupServiceImplTest extends EntityServiceTest with KvTest {
   private val groupId = RandomService().uuid
   private val group = GroupModel(groupId, users, Option(time), Option(time), eventCount)
 
-  private val userMock = new UserServiceMock(getUsersResult = Future.successful(Right(users)))
-
   private val method = new service.GetGroupMethod(groupId)
 
   override def beforeAll() {
     super.beforeAll()
     Services.register(TimeServiceConstantMock)
-    Services.register(userMock)
   }
 
   test("getGroup - success") {
+    val userService = mock[UserService]
+    (userService.getUsers(_: List[UUID])(_: Pipeline, _: AuthContext)) expects
+      (*, *, *) returning Future.successful(Right(users))
+    val unregister = TestServices.register(userService)
+
     assert(GroupUserKeyService().make(groupId, authCtx.userId).setLastRead(time).await())
     assert(GroupKeyService().make(KeyId(groupId)).setLastEvent(time).await())
     assert(GroupKeyService().make(KeyId(groupId)).setEventCount(eventCount).await())
 
     assert(service.getGroup(groupId).await() == Right(Option(group)))
+
+    unregister()
   }
 
   test("getGroup - users get failed") {
@@ -63,7 +70,10 @@ class GroupServiceImplTest extends EntityServiceTest with KvTest {
     val userId = RandomService().uuid
     val userIds = List(userId)
     val builder = GroupModelBuilder()
-    val unregister = TestServices.register(new UserServiceMock(getUsersResult = Future.successful(Left(error))))
+    val userService = mock[UserService]
+    (userService.getUsers(_: List[UUID])(_: Pipeline, _: AuthContext)) expects
+      (*, *, *) returning Future.successful(Left(error))
+    val unregister = TestServices.register(userService)
     assert(method.usersGet(userIds, builder).await() == Left(error))
     unregister()
   }
--- project-entity/src/test/scala/base/entity/user/impl/LoginCommandServiceImplTest.scala
+++ project-entity/src/test/scala/base/entity/user/impl/LoginCommandServiceImplTest.scala
@@ -2,7 +2,7 @@
  * Copyright (c) 2015 Robert Conrad - All Rights Reserved.
  * Unauthorized copying of this file, via any medium is strictly prohibited.
  * This file is proprietary and confidential.
- * Last modified by rconrad, 1/18/15 4:02 PM
+ * Last modified by rconrad, 1/18/15 4:15 PM
  */
 
 package base.entity.user.impl
@@ -18,7 +18,7 @@ import base.entity.auth.context.{ AuthContext, AuthContextDataFactory }
 import base.entity.command.impl.CommandServiceImplTest
 import base.entity.device.model.DeviceModel
 import base.entity.error.ApiError
-import base.entity.group.GroupEventsService
+import base.entity.group.{ GroupEventsService, UserService }
 import base.entity.kv.Key._
 import base.entity.kv.PrivateHashKey
 import base.entity.kv.impl.PrivateHashKeyImpl
@@ -28,7 +28,6 @@ import base.entity.question.model.AnswerModel
 import base.entity.user.impl.LoginCommandServiceImpl._
 import base.entity.user.kv.UserKeyProps._
 import base.entity.user.kv.impl.{ DeviceKeyImpl, UserKeyImpl }
-import base.entity.user.mock.UserServiceMock
 import base.entity.user.model.{ LoginModel, LoginResponseModel }
 
 import scala.concurrent.Future
@@ -57,7 +56,6 @@ class LoginCommandServiceImplTest extends CommandServiceImplTest {
   override def beforeAll() {
     super.beforeAll()
     Services.register(TimeServiceConstantMock)
-    Services.register(new UserServiceMock())
     registerQuestionMock()
   }
 
@@ -134,7 +132,10 @@ class LoginCommandServiceImplTest extends CommandServiceImplTest {
   }
 
   test("failed to get groups") {
-    val unregister = TestServices.register(new UserServiceMock(getGroupsResult = Future.successful(Left(apiError))))
+    val userService = mock[UserService]
+    (userService.getGroups(_: UUID)(_: Pipeline, _: AuthContext)) expects
+      (*, *, *) returning Future.successful(Left(apiError))
+    val unregister = TestServices.register(userService)
     assert(command.groupsGet(RandomService().uuid).await() == Left(apiError))
     unregister()
   }
