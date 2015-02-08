42f09bbf1c02ba807c2131445849e6334859706b
robert.conrad <robert.conrad@coinposit.com>
2015-02-07 22:29:26 -0800

    Don't double sprintf SocketLoggable log lines

--- project-socket/src/main/scala/base/socket/logging/SocketLoggable.scala
+++ project-socket/src/main/scala/base/socket/logging/SocketLoggable.scala
@@ -2,7 +2,7 @@
  * Copyright (c) 2015 Robert Conrad - All Rights Reserved.
  * Unauthorized copying of this file, via any medium is strictly prohibited.
  * This file is proprietary and confidential.
- * Last modified by rconrad, 1/17/15 1:52 PM
+ * Last modified by rconrad, 2/7/15 10:28 PM
  */
 
 package base.socket.logging
@@ -17,33 +17,35 @@ import base.common.logging.Loggable
  */
 trait SocketLoggable extends Loggable {
 
+  private def prependAuthCtx(x: Seq[Any])(implicit ch: LoggableChannelInfo) =
+    Seq(ch.authCtx.user.map(_.id), ch.remoteAddress) ++ x
+
+  private def prependFormat(msg: String): String = "User:%s Addr:%s " + msg
+
   protected def trace(msg: String, x: Any*)(implicit ch: LoggableChannelInfo) {
-    if (isTraceEnabled) super.trace(format(msg, x: _*))
+    if (isTraceEnabled) super.trace(prependFormat(msg), prependAuthCtx(x): _*)
   }
 
   protected def debug(msg: String, x: Any*)(implicit ch: LoggableChannelInfo) {
-    if (isDebugEnabled) super.debug(format(msg, x: _*))
+    if (isDebugEnabled) super.debug(prependFormat(msg), prependAuthCtx(x): _*)
   }
 
   protected def info(msg: String, x: Any*)(implicit ch: LoggableChannelInfo) {
-    if (isInfoEnabled) super.info(format(msg, x: _*))
+    if (isInfoEnabled) super.info(prependFormat(msg), prependAuthCtx(x): _*)
   }
 
   protected def warn(msg: String, x: Any*)(implicit ch: LoggableChannelInfo) {
-    if (isWarnEnabled) super.warn(format(msg, x: _*))
+    if (isWarnEnabled) super.warn(prependFormat(msg), prependAuthCtx(x): _*)
   }
   protected def warn(msg: String, e: Throwable, x: Any*)(implicit ch: LoggableChannelInfo) {
-    if (isWarnEnabled) super.warn(format(msg, x: _*), e)
+    if (isWarnEnabled) super.warn(prependFormat(msg), e, prependAuthCtx(x): _*)
   }
 
   protected def error(msg: String, x: Any*)(implicit ch: LoggableChannelInfo) {
-    if (isErrorEnabled) super.error(format(msg, x: _*))
+    if (isErrorEnabled) super.error(prependFormat(msg), prependAuthCtx(x): _*)
   }
   protected def error(msg: String, e: Throwable, x: Any*)(implicit ch: LoggableChannelInfo) {
-    if (isErrorEnabled) super.error(format(msg, x: _*), e)
+    if (isErrorEnabled) super.error(prependFormat(msg), e, prependAuthCtx(x): _*)
   }
-
-  private def format(msg: String, x: Any*)(implicit ch: LoggableChannelInfo): String =
-    super.format(s"User:${ch.authCtx.user.map(_.id)} Addr:${ch.remoteAddress} $msg", x: _*)
 
 }
