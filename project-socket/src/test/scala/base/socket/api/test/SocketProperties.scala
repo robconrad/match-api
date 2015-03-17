844d8fa374da88a5388fd4a62087126b78026b6a
robert.conrad <robert.conrad@coinposit.com>
2015-03-16 20:45:05 -0700

    Copyright updates didn't take somehow

--- project-socket/src/test/scala/base/socket/api/test/SocketProperties.scala
+++ project-socket/src/test/scala/base/socket/api/test/SocketProperties.scala
@@ -2,7 +2,7 @@
  * Copyright (c) 2015 Robert Conrad - All Rights Reserved.
  * Unauthorized copying of this file, via any medium is strictly prohibited.
  * This file is proprietary and confidential.
- * Last modified by rconrad, 3/15/15 10:39 AM
+ * Last modified by rconrad, 3/15/15 10:50 AM
  */
 
 package base.socket.api.test
